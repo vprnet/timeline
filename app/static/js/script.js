c1bde029a6ac02c67d23114d71e20ec4b6df71e7
SARA SIMON <smbsimon@gmail.com>
2016-08-29 11:55:52 -0400

    minor style changes to text styles and chevron arrows

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -77,7 +77,7 @@ var canPlay = function(audio, progressBar, fullWidth, duration, remaining, glyph
 
 
 $(':header').addClass('headline');
-$('h5').hide().fadeIn(19500);
+$('h5').hide().fadeIn(13500);
 $('h5').on('click', function() {
   $(this).remove();
 });
